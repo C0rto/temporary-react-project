673b291044675923cbbe2ebdd5b500f71d489383
Cort0 <luimadex@gmail.com>
2022-12-03 18:33:22 +0100

    info upd

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,9 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Questa Web App è un semplice iterfaccia per cercare cocktails. I dati
-        utilizzati provengono da{' '}
+        Questa è un semplice Web App per cercare cocktails. Scrivi il nome di un
+        cocktail o le lettere che ricordi del cocktail che desideri, la ricerca
+        automatica farà il resto. <br />I dati utilizzati provengono da{' '}
         <a href="https://www.thecocktaildb.com/">TheCocktailDB</a> e dalla loro
         API gratuita.
       </p>
